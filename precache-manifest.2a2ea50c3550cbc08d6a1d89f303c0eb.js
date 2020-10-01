70355efd046510bdb0aab4c96afaf1d7458741a7
rajeev ranjan singh <rajeevranjan54545@gmail.com>
2020-10-01 06:17:39 +0530

    Updates

---
+++ precache-manifest.2a2ea50c3550cbc08d6a1d89f303c0eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db604cb8f9c1d5d7646740f3be56372",
+    "url": "/my-app1/index.html"
+  },
+  {
+    "revision": "d16d1a153ce6858d1761",
+    "url": "/my-app1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/my-app1/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app1/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16d1a153ce6858d1761",
+    "url": "/my-app1/static/js/main.3f1dc6aa.chunk.js"
+  },
+  {
+    "revision": "5bfba0f97404a38d86a3",
+    "url": "/my-app1/static/js/runtime-main.74faeb19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
